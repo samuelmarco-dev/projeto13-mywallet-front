24971d062d649bfb784bfbbf30ef5bd2f3104a89
Rodrigodcs <rodrigodcsee@gmail.com>
2022-05-20 12:08:06 -0300

    Preparando revisão de código

--- src/components/utils/Botao.js
+++
@@ -1,9 +0,0 @@
-function Botao(props) {
-    const {classe, conteudo, click, tipo, disabled} = props;
-
-    return ( 
-        <button className={classe} type={tipo} onClick={click} disabled={disabled}>{conteudo}</button>
-    );
-}
-
-export default Botao;
